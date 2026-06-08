713148b4abfc14937b9d26a7ee9a556f0c2150e2
availableblank <yexiao105835140@gmail.com>
2026-06-08 15:16:51 +0800

    更新 strings.dart

--- lib/common/constants/strings.dart
+++ lib/common/constants/strings.dart
@@ -1,6 +1,6 @@
 class Strings {
   // App
-  static const String appName = 'asmr.one';
+  static const String appName = 'ASMROne';
 
   // Common
   static const String loading = '加载中...';
@@ -25,4 +25,16 @@ class Strings {
   static const String home = '主页';
   static const String favorites = '我的收藏';
   static const String settings = '设置';
+  
+  //History
+  static const String history = '历史记录';
+  static const String clearAllHistory = '清空全部记录';
+  static const String historyEmpty = '暂无播放历史';
+  static const String historySearchHint = '搜索历史记录...';
+  static const String deleteHistoryConfirm = '确定要清空所有历史记录吗？';
+  static const String maxHistoryCount = '历史记录上限';
+  static const String maxHistoryCountDesc = '超过上限自动清除最早记录';
+  static const String cancel = '取消';
+  static const String confirm = '确定';
+  static const String delete = '删除';
 }
